597953a40734c4c40093c4e65d23b94c2999c51d
unknown <mvegaplm@gmail.com>
2019-01-03 17:59:13 -0500

    init project

---
+++ precache-manifest.b8f761e80efa352a288b5b864d8a93a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d63c6109ca7999b5c351",
+    "url": "/static/js/main.d63c6109.chunk.js"
+  },
+  {
+    "revision": "bc3657e023ee1862bbb9",
+    "url": "/static/js/1.bc3657e0.chunk.js"
+  },
+  {
+    "revision": "d63c6109ca7999b5c351",
+    "url": "/static/css/main.bf0115ac.chunk.css"
+  },
+  {
+    "revision": "8505367bab5486ebe0a38ebb73253e5a",
+    "url": "/index.html"
+  }
+];
